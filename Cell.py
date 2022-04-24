1b5937664afd6ba89617549292e86cecac30c81e
Phan Vũ Tuấn Anh <88922348+KiteryuKatori@users.noreply.github.com>
2022-04-25 00:17:25 +0700

    Cell Class: change and update defineSelf() to status() + update set/removePiece()

--- Cell.py
+++ Cell.py
@@ -2,27 +2,32 @@
 
 class Cell:
 	def __init__(self, loc):
-		print("Cell class says hi.")
 		# Parameters:
-		# 	- loc: list of 2 integers
+		# 	- loc: list of 2 integers 
 		# 		tells the location of the cells
 		self.loc 				= loc
+		print(f"Cell class says hi at {self.loc}")
+		self.isOccupied			= False
 		self.isTransformable 	= False
 		self.Score 				= 0
 		self.isAvailable		= False
 		self.isBlack			= ((loc[0] + loc[1]) % 2) == 0
-		self.piece				= "" # chess piece objects
+		self.piece				= None # chess piece objects
 	
 	def setPiece(self, Piece):
+		self.isOccupied = True
 		self.piece = Piece
 
 	def removePiece(self):
-		self.piece = ""
-
-	def showPiece(self):
-		return self.piece
+		self.isOccupied = True
+		self.piece = None
 
 	def defineSelf(self):
-		print(f"Loc = {self.loc}, Piece = {self.piece}")
-		# print(f"Loc = {self.loc}, Piece = {self.piece.name}") how to return self.piece as an empty object without importing EM? 
+		# assert self.piece == None, "The Cell is empty" 
+		# the code above didn't work, why?
+
+		if self.piece == None:
+			print(f"Loc = {self.loc}, The cell is empty")
+			return
+		print(f"Loc = {self.loc}, Piece = {self.piece.name}")
 
--- main.py
+++ main.py
@@ -7,10 +7,3 @@ from Piece import *
 # board = Board()
 cell  = Cell((5, 6))
 Piece = Piece("king", True)
-
-# cell.setPiece(rookBlack)
-# cell.removePiece()
-# cell.defineSelf()
-# cell.setPiece(rookWhite)
-# cell.showPiece()
-# cell.defineSelf()

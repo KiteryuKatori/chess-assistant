6c3daa129fbb1f4d2db33bf26c55b7b18979ed1f
Tri Phung <cadbane1998@gmail.com>
2022-05-07 12:26:10 +0300

    style(chess_board_scrap.py): rename variable for naming convetion

--- chess_board_scrap.py
+++ chess_board_scrap.py
@@ -97,7 +97,7 @@ board[6][5] = Piece("pawn",   False)
 board[6][6] = Piece("pawn",   False)
 board[6][7] = Piece("pawn",   False)
 
-board_visualized = []
+boardVisualized = []
 
 for row in board:
     row_visualized = []
@@ -107,7 +107,7 @@ for row in board:
         else:
             row_visualized.append('')
     
-    board_visualized.append(row_visualized)
+    boardVisualized.append(row_visualized)
 
 # for row in board_update:
 #     for piece in row:
@@ -115,4 +115,4 @@ for row in board:
 #         print(piece, end = ' ')
 #     print()    
     
-printBoardWPiece(board_visualized)
+printBoardWPiece(boardVisualized)
